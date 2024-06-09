a3370de7e2bdd8bc07eb1dc72cfda991bcd043e1
bepisie <98575711+bepisie@users.noreply.github.com>
2024-06-09 14:55:45 +0630

    Create root.sh

---
+++ root.sh
@@ -0,0 +1,27 @@
+if [ -d "./sys" ] 
+then
+./dist/proot -S . /bin/bash
+else
+echo "###############################"
+echo "Render Debian"
+echo "###############################"
+echo ":: Installing Debian ::"
+sleep 2s
+clear
+wget -O akuh.zip https://media.githubusercontent.com/media/akuhnet/wqemu/master/akuh.zip
+clear
+echo "###############################"
+echo "## Render Debian ##"
+echo "###############################"
+echo ":: Installing Debian ::"
+echo ":: Setting up Debian ::"
+unzip akuh.zip
+unzip root.zip
+tar -xvf root.tar.xz
+rm -rf akuh.zip root.zip root.tar.xz
+clear
+echo "-: Welcome to Render (Debian)! :-"
+echo "-: It is highly reccomended you run pkg.sh first before Installing anything. :-"
+echo "-: I added "toys" to Debian that include neofetch and cmatrix. :-"
+./dist/proot -S . /bin/bash
+fi
